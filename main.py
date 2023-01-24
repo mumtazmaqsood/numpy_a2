9482e4d6021374e56701db4a6f1c3926396b9c60
mumtazmaqsood <mumtaz.maqsood@yahoo.com>
2023-01-24 16:12:19 +0100

    3d array added

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import numpy as np
 
 #creating array
 x = np.array([22,33,44,55])
-print(x)
+# print(x)
 #get the size of the array
-print(x.size)
+# print(x.size)
 #get the shape & dimission
-print(x.shape, x.ndim)
+# print(x.shape, x.ndim)
 
 #can be create by using arange function
-print(np.arange(10))
+# print(np.arange(10))
 #---------------------------------------------------------
 
 #creating 2dimissional array
@@ -23,7 +23,17 @@ d2_array = np.array([[1,2,3], [11,22,33], [111,222,333], [1111,2222,3333]])
 # arr2d = np.arange(20).reshape(1,20)
 # arr2d = np.arange(20).reshape(20,1)
 # arr2d = np.arange(20).reshape(4,5)
-arr2d = np.arange(20).reshape(5,4)
+# arr2d = np.arange(20).reshape(5,4)
 # arr2d = np.arange(20).reshape(2,10)
 # arr2d = np.arange(20).reshape(10,2)
-print(arr2d)
+# print(arr2d)
+
+#--------------------------------------------------------------------
+
+#creating 3dmissional array
+
+arr3d = np.arange(64).reshape(4,4,4)
+print(arr3d)
+
+#vactorlized operations --> mulitplying 3d array with 2 and all the elements will multiplied by 2
+print(arr3d*2)
