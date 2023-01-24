5d3e1df6fa8fbea6b960d0daca23359846dcf210
mumtazmaqsood <mumtaz.maqsood@yahoo.com>
2023-01-24 13:46:31 +0100

    creating 1demissional array

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+#creating array
+x = np.array([22,33,44,55])
+print(x)
+#get the size of the array
+print(x.size)
+#get the shape & dimission
+print(x.shape, x.ndim)
+
+#can be create by using arange function
+print(np.arange(10))
