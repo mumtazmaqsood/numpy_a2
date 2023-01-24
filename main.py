2cc630aca60aa054a453d7ea4edcff2a668fd24b
mumtazmaqsood <mumtaz.maqsood@yahoo.com>
2023-01-24 14:10:14 +0100

    2dmissional array created with different function(array, arange, reshape etc)

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ print(x.shape, x.ndim)
 
 #can be create by using arange function
 print(np.arange(10))
+#---------------------------------------------------------
+
+#creating 2dimissional array
+
+d2_array = np.array([[1,2,3], [11,22,33], [111,222,333], [1111,2222,3333]])
+# print(d2_array)
+# print(d2_array.ndim)
+
+#creating 2dimissional array by using arange & reshape functions
+# arr2d = np.arange(20).reshape(1,20)
+# arr2d = np.arange(20).reshape(20,1)
+# arr2d = np.arange(20).reshape(4,5)
+arr2d = np.arange(20).reshape(5,4)
+# arr2d = np.arange(20).reshape(2,10)
+# arr2d = np.arange(20).reshape(10,2)
+print(arr2d)
